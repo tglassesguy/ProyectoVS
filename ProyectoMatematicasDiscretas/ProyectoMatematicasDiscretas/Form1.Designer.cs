f8383d60af38e024f3c00f297c39ced6fafb978a
Javier Collazos <jcollazosgomez@gmail.com>
2018-02-15 22:35:43 -0500

    pull para revisar master

--- ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.Designer.cs
+++ ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.Designer.cs
@@ -38,6 +38,22 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btmAgregarFinal = new System.Windows.Forms.Button();
+            this.btmAgregarAUnPunto = new System.Windows.Forms.Button();
+            this.txtPosicionAgregar = new System.Windows.Forms.TextBox();
+            this.btmAgregarInicio = new System.Windows.Forms.Button();
+            this.pnlPanelCargar = new System.Windows.Forms.Panel();
+            this.btmSiguiente = new System.Windows.Forms.Button();
+            this.btmAnterior = new System.Windows.Forms.Button();
+            this.btmCargarFinal = new System.Windows.Forms.Button();
+            this.btmCargarInicio = new System.Windows.Forms.Button();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.btmBusqueda = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.RITtextos = new System.Windows.Forms.RichTextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.pnlPanelCargar.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -52,7 +68,7 @@
             // lblSubtitulo
             // 
             this.lblSubtitulo.AutoSize = true;
-            this.lblSubtitulo.Location = new System.Drawing.Point(54, 122);
+            this.lblSubtitulo.Location = new System.Drawing.Point(50, 82);
             this.lblSubtitulo.Name = "lblSubtitulo";
             this.lblSubtitulo.Size = new System.Drawing.Size(146, 17);
             this.lblSubtitulo.TabIndex = 1;
@@ -61,7 +77,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 155);
+            this.label1.Location = new System.Drawing.Point(50, 115);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 17);
             this.label1.TabIndex = 2;
@@ -70,7 +86,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 187);
+            this.label2.Location = new System.Drawing.Point(50, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 3;
@@ -79,7 +95,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(54, 218);
+            this.label3.Location = new System.Drawing.Point(50, 178);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 17);
             this.label3.TabIndex = 4;
@@ -88,7 +104,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(54, 248);
+            this.label4.Location = new System.Drawing.Point(50, 208);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 17);
             this.label4.TabIndex = 5;
@@ -96,37 +112,170 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(168, 152);
+            this.textBox1.Location = new System.Drawing.Point(164, 112);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(139, 22);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(168, 184);
+            this.textBox2.Location = new System.Drawing.Point(164, 144);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(139, 22);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(168, 215);
+            this.textBox3.Location = new System.Drawing.Point(164, 175);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(139, 22);
             this.textBox3.TabIndex = 8;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(168, 245);
+            this.textBox4.Location = new System.Drawing.Point(164, 205);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(139, 22);
             this.textBox4.TabIndex = 9;
+            // 
+            // btmAgregarFinal
+            // 
+            this.btmAgregarFinal.Location = new System.Drawing.Point(497, 134);
+            this.btmAgregarFinal.Name = "btmAgregarFinal";
+            this.btmAgregarFinal.Size = new System.Drawing.Size(121, 42);
+            this.btmAgregarFinal.TabIndex = 11;
+            this.btmAgregarFinal.Text = "Agregar al final";
+            this.btmAgregarFinal.UseVisualStyleBackColor = true;
+            // 
+            // btmAgregarAUnPunto
+            // 
+            this.btmAgregarAUnPunto.Location = new System.Drawing.Point(488, 182);
+            this.btmAgregarAUnPunto.Name = "btmAgregarAUnPunto";
+            this.btmAgregarAUnPunto.Size = new System.Drawing.Size(139, 42);
+            this.btmAgregarAUnPunto.TabIndex = 12;
+            this.btmAgregarAUnPunto.Text = "Agregar a un punto";
+            this.btmAgregarAUnPunto.UseVisualStyleBackColor = true;
+            // 
+            // txtPosicionAgregar
+            // 
+            this.txtPosicionAgregar.Location = new System.Drawing.Point(633, 192);
+            this.txtPosicionAgregar.Name = "txtPosicionAgregar";
+            this.txtPosicionAgregar.Size = new System.Drawing.Size(43, 22);
+            this.txtPosicionAgregar.TabIndex = 13;
+            // 
+            // btmAgregarInicio
+            // 
+            this.btmAgregarInicio.Location = new System.Drawing.Point(497, 82);
+            this.btmAgregarInicio.Name = "btmAgregarInicio";
+            this.btmAgregarInicio.Size = new System.Drawing.Size(121, 45);
+            this.btmAgregarInicio.TabIndex = 14;
+            this.btmAgregarInicio.Text = "Agregar al inicio";
+            this.btmAgregarInicio.UseVisualStyleBackColor = true;
+            // 
+            // pnlPanelCargar
+            // 
+            this.pnlPanelCargar.BackColor = System.Drawing.Color.LightGray;
+            this.pnlPanelCargar.Controls.Add(this.panel1);
+            this.pnlPanelCargar.Controls.Add(this.btmBusqueda);
+            this.pnlPanelCargar.Controls.Add(this.txtBusqueda);
+            this.pnlPanelCargar.Controls.Add(this.btmCargarInicio);
+            this.pnlPanelCargar.Controls.Add(this.btmSiguiente);
+            this.pnlPanelCargar.Controls.Add(this.btmAnterior);
+            this.pnlPanelCargar.Controls.Add(this.btmCargarFinal);
+            this.pnlPanelCargar.Location = new System.Drawing.Point(10, 243);
+            this.pnlPanelCargar.Name = "pnlPanelCargar";
+            this.pnlPanelCargar.Size = new System.Drawing.Size(727, 285);
+            this.pnlPanelCargar.TabIndex = 15;
+            // 
+            // btmSiguiente
+            // 
+            this.btmSiguiente.Location = new System.Drawing.Point(126, 210);
+            this.btmSiguiente.Name = "btmSiguiente";
+            this.btmSiguiente.Size = new System.Drawing.Size(104, 32);
+            this.btmSiguiente.TabIndex = 3;
+            this.btmSiguiente.Text = "--->";
+            this.btmSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // btmAnterior
+            // 
+            this.btmAnterior.Location = new System.Drawing.Point(16, 210);
+            this.btmAnterior.Name = "btmAnterior";
+            this.btmAnterior.Size = new System.Drawing.Size(104, 32);
+            this.btmAnterior.TabIndex = 2;
+            this.btmAnterior.Text = "<---";
+            this.btmAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btmCargarFinal
+            // 
+            this.btmCargarFinal.Location = new System.Drawing.Point(22, 103);
+            this.btmCargarFinal.Name = "btmCargarFinal";
+            this.btmCargarFinal.Size = new System.Drawing.Size(205, 70);
+            this.btmCargarFinal.TabIndex = 1;
+            this.btmCargarFinal.Text = "Final";
+            this.btmCargarFinal.UseVisualStyleBackColor = true;
+            // 
+            // btmCargarInicio
+            // 
+            this.btmCargarInicio.Location = new System.Drawing.Point(22, 27);
+            this.btmCargarInicio.Name = "btmCargarInicio";
+            this.btmCargarInicio.Size = new System.Drawing.Size(205, 70);
+            this.btmCargarInicio.TabIndex = 4;
+            this.btmCargarInicio.Text = "Inicio";
+            this.btmCargarInicio.UseVisualStyleBackColor = true;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Location = new System.Drawing.Point(16, 257);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(170, 22);
+            this.txtBusqueda.TabIndex = 5;
+            // 
+            // btmBusqueda
+            // 
+            this.btmBusqueda.Location = new System.Drawing.Point(194, 257);
+            this.btmBusqueda.Name = "btmBusqueda";
+            this.btmBusqueda.Size = new System.Drawing.Size(36, 22);
+            this.btmBusqueda.TabIndex = 6;
+            this.btmBusqueda.Text = "♀";
+            this.btmBusqueda.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.lblTotal);
+            this.panel1.Controls.Add(this.RITtextos);
+            this.panel1.Location = new System.Drawing.Point(284, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(438, 258);
+            this.panel1.TabIndex = 7;
+            // 
+            // RITtextos
+            // 
+            this.RITtextos.Location = new System.Drawing.Point(26, 19);
+            this.RITtextos.Name = "RITtextos";
+            this.RITtextos.Size = new System.Drawing.Size(346, 197);
+            this.RITtextos.TabIndex = 0;
+            this.RITtextos.Text = "";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(359, 231);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(23, 17);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "---";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(744, 534);
+            this.Controls.Add(this.pnlPanelCargar);
+            this.Controls.Add(this.btmAgregarInicio);
+            this.Controls.Add(this.txtPosicionAgregar);
+            this.Controls.Add(this.btmAgregarAUnPunto);
+            this.Controls.Add(this.btmAgregarFinal);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -139,6 +288,10 @@
             this.Controls.Add(this.lblTitulo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.pnlPanelCargar.ResumeLayout(false);
+            this.pnlPanelCargar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +309,20 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btmAgregarFinal;
+        private System.Windows.Forms.Button btmAgregarAUnPunto;
+        private System.Windows.Forms.TextBox txtPosicionAgregar;
+        private System.Windows.Forms.Button btmAgregarInicio;
+        private System.Windows.Forms.Panel pnlPanelCargar;
+        private System.Windows.Forms.Button btmSiguiente;
+        private System.Windows.Forms.Button btmAnterior;
+        private System.Windows.Forms.Button btmCargarFinal;
+        private System.Windows.Forms.Button btmCargarInicio;
+        private System.Windows.Forms.Button btmBusqueda;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.RichTextBox RITtextos;
     }
 }
 
