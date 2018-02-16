59333bf18af5cd9fd2a5e0dbbeb57b28c4dfad78
Javier Collazos <jcollazosgomez@gmail.com>
2018-02-15 22:30:45 -0500

    cambios en la clase mundo

--- ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.cs
+++ ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.cs
@@ -8,13 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ProyectoMatematicasDiscretas
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+
+            mundo casa = new mundo();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
--- ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/mundo.cs
+++ ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/mundo.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace ProyectoMatematicasDiscretas
 {
+    public static int MAX_NOMBRE = 10;
+    public static int MAX_NUMEROPAQUETES = 10;
+    public static int MAX_NUMEROPAQUETES = 10;
+
+
+    FileStream archivo;
+
     class mundo
     {
+
+    }
+
+    public void aviso()
+    {
+     
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
