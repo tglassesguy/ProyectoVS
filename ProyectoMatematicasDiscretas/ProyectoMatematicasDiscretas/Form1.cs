e23b76a2dcd090a4b83c10508d8ea2668835f00d
Javier Collazos <jcollazosgomez@gmail.com>
2018-02-15 22:12:08 -0500

    Creación de la clase mundo

--- ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.cs
+++ ProyectoMatematicasDiscretas/ProyectoMatematicasDiscretas/Form1.cs
@@ -16,5 +16,10 @@ namespace ProyectoMatematicasDiscretas
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
